155238a77ba1b9c82656ed4cedfd00cc747e6cdd
zarraga <zarraga@10.109.1.145>
2016-12-12 19:01:21 +0100

    yeah

--- flickr/src/isad/flickr/backend/ArgazkienTaula.java
+++ flickr/src/isad/flickr/backend/ArgazkienTaula.java
@@ -1,19 +1,33 @@
 package isad.flickr.backend;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class ArgazkienTaula extends JFrame {
+	JButton Igo = new JButton("Igo");
+	MyTableModel modeloa = new MyTableModel();
 
 	public ArgazkienTaula() {
 		super("Nire taula grafikoa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JTable table = new JTable(new MyTableModel());
+		table.setRowHeight(75);
 		JScrollPane scrollPane = new JScrollPane(table);
-		setContentPane(scrollPane);
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		getContentPane().add(Igo, BorderLayout.SOUTH);	
 		pack();
 		setVisible(true);
+		Igo.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				modeloa = (MyTableModel) table.getModel();
+			}});
 	}
 	
 	public static void main(String[] args) {
